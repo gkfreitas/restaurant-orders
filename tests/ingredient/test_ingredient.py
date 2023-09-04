59a42ae62ac1b1886d0e05bfe0331f6aca155e72
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-09-04 11:24:10 -0300

    REQ01 - Finalizado, testes para o class ingredients finalizado

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_name = "frango"
+    different_ingredient_name = "carne"
+    instance = Ingredient(ingredient_name)
+    different_instance = Ingredient(different_ingredient_name)
+    assert instance.__hash__() == hash(ingredient_name)
+    assert instance.__hash__() != hash(different_instance)
+    assert instance.name == ingredient_name
+    assert instance.name != different_ingredient_name
+    assert instance != different_instance
+    assert instance == instance
+    assert instance.__repr__() == f"Ingredient('{instance.name}')"
+    assert instance.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
